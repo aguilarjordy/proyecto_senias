3f8f964409de1ba85015044904fb9e55df05b9ae
sebascord <cordova01608@gmail.com>
2025-09-26 16:11:15 -0500

    2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Principal from '../Principal';  // Cambia esta línea
+import Principal from '../Principal.jsx';  // Cambia esta línea
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
